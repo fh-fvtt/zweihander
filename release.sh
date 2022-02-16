c57c64013bd5ab234666090f26ea75513166f02e
kxfin <mail@kxfin.xyz>
2022-02-16 18:07:48 +0100

    implement CI

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+git diff --exit-code &>/dev/null
+if [ $? -ne 0 ] && [ "$2" != "--force" ]; then
+  echo "You have local changes, which might need to be commited before the release. To continue, commit your changes or pass --force as second argument."
+  exit 1
+fi
+git diff --cached --exit-code &>/dev/null
+if [ $? -ne 0 ] && [ "$2" != "--force" ]; then
+  echo "You have staged changes, which might need to be commited before the relesae. To continue, commit your changes or pass --force as second argument."
+  exit 1
+fi
+version=$1
+manifest_branch=$(git branch --show-current)
+cat system.template.json | sed -e 's/{{version}}/'"${version}"'/g' -e 's/{{manifest}}/https:\/\/raw.githubusercontent.com\/fh-fvtt\/zweihander\/'"${manifest_branch}"'\/system.json/g' > system.json
+# git tag "v${version}"
+# git push
