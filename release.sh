974ff7062218038e69f0e41a5be17e74a4437505
kxfin <mail@kxfin.xyz>
2022-02-16 22:28:00 +0100

    update release script

--- release.sh
+++ release.sh
@@ -5,13 +5,15 @@ if [ $? -ne 0 ] && [ "$2" != "--force" ]; then
   exit 1
 fi
 git diff --cached --exit-code &>/dev/null
-if [ $? -ne 0 ] && [ "$2" != "--force" ]; then
-  echo "You have staged changes, which might need to be commited before the relesae. To continue, commit your changes or pass --force as second argument."
+if [ $? -ne 0 ]; then
+  echo "You have staged changes, the release process can not continue."
   exit 1
 fi
 version=$1
 manifest_branch=$(git branch --show-current)
 cat system.template.json | sed -e 's/{{version}}/'"${version}"'/g' -e 's/{{manifest}}/https:\/\/raw.githubusercontent.com\/fh-fvtt\/zweihander\/'"${manifest_branch}"'\/system.json/g' > system.json
 git tag "v${version}"
+git add system.json
+git commit -m "preparing release v${version}"
 git push
 git push origin "v${version}"
