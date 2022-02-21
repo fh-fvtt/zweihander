c975fa6be1c89f3ac798b0919c696cc0953b8c7e
kxfin <mail@kxfin.xyz>
2022-02-21 11:09:47 +0100

    fix build process

--- rollup.config.js
+++ rollup.config.js
@@ -17,9 +17,9 @@ if you want to view the source visit the plugins github repository
 const dev = process.env.PRODUCTION_MODE === undefined;
 
 export default {
-  input: "./module/main.js",
+  input: "./module/zweihander.js",
   output: {
-    file: "zweihander.js",
+    dir: dev ? "." : "./dist/",
     sourcemap: true,
     sourcemapExcludeSources: dev,
     format: "es",
