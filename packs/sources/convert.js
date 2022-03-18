5765faba823e93d387ffb421fc4aed2e05bd8341
kvndrsslr <kvndrsslr@gmail.com>
2022-03-18 13:48:22 +0100

    add creature traits

--- packs/sources/convert.js
+++
@@ -1,9 +0,0 @@
-let csvToJson = require('convert-csv-to-json');
-
-let json = csvToJson
-  .fieldDelimiter(',')
-  .formatValueByType()
-  .getJsonFromCsv("ancestral-traits.csv");
-for(let i=0; i<json.length;i++){
-    console.log(JSON.stringify(json[i]));
-}
