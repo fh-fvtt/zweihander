d0044288bf316dfbbf946b57630fde2948ca0dfe
re4xn <sa_veliy@hotmail.com>
2022-05-09 13:06:58 +0100

    Fix Injury creation from sheet

--- module/item/entity/injury.js
+++ module/item/entity/injury.js
@@ -4,7 +4,8 @@ export default class ZweihanderInjury extends ZweihanderBaseItem {
 
   async _preCreate(data, options, user, that) {
     await super._preCreate(data, options, user);
-    if (that.actor && data.data.recuperationTime === 0) {
+
+    if (that.actor && data?.data?.recuperationTime === 0) {
       const d = data.data.severity + 1;
       const roll = await (new Roll(`${d}d10+${d}`)).evaluate();
       const speaker = ChatMessage.getSpeaker({actor: that.actor});
