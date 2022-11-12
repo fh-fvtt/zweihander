40e5133a32a08e8c15eb807fd438eba1c7573ecf
kvndrsslr <kvndrsslr@gmail.com>
2022-11-12 12:34:12 +0100

    fix bug preventing injuries being created on actor

--- src/module/item/entity/injury.js
+++ src/module/item/entity/injury.js
@@ -5,7 +5,7 @@ export default class ZweihanderInjury extends ZweihanderBaseItem {
     await super._preCreate(item, options, user);
 
     if (that.actor && item?.system?.recuperationTime === 0) {
-      const d = Number(system.severity) + 1;
+      const d = Number(item.system.severity) + 1;
       const roll = await new Roll(`${d}d10+${d}`).evaluate();
       const speaker = ChatMessage.getSpeaker({ actor: that.actor });
       roll.toMessage({ flavor: 'Determining Recuperation Time', speaker });
