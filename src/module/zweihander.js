d4e6a0f8d029204ec488ce7d074b7b61edd90001
Jose Lozano <lozanoje@gmail.com>
2023-01-22 17:47:56 +0100

    Added drag ruler integration

--- src/module/zweihander.js
+++ src/module/zweihander.js
@@ -97,6 +97,42 @@ Hooks.once('diceSoNiceReady', function () {
   ]);
 });
 
+Hooks.once("dragRuler.ready", (SpeedProvider) => {
+  // Drag Ruler integration
+    class zweihanderSpeedProvider extends SpeedProvider {
+        get colors() {
+            return [
+                {id: "maneuver", default: 0x1259b6, name: "zweihander.speeds.maneuver"},
+                {id: "hustle", default: 0x00FF00, name: "zweihander.speeds.hustle"},
+                {id: "charge", default: 0xFFFF00, name: "zweihander.speeds.charge"},
+                {id: "run", default: 0xFF8000, name: "zweihander.speeds.run"}
+            ];
+        };
+
+        getRanges(token) {
+			// MANEUVER: 1 yd
+			// HUSTLE: MOV
+			// CHARGE: MOV*2
+			// RUN: MOV*3
+			
+			const minSpeed = 1;
+            const baseSpeed = token.actor.system.stats.secondaryAttributes.movement.current;
+            const chargeSpeed = baseSpeed * 2;
+            const runSpeed = baseSpeed * 3;
+			
+            const ranges = [
+                {range: minSpeed, color: "maneuver"},
+                {range: baseSpeed, color: "hustle"},
+                {range: chargeSpeed, color: "charge"},
+                {range: runSpeed, color: "run"}
+            ];
+
+            return ranges;
+        };
+    }
+    dragRuler.registerModule("fvtt-module-drag-ruler-zweihander", zweihanderSpeedProvider);
+})
+
 Hooks.once('init', async function () {
   // CONFIG.debug.hooks = true;
   console.log(ZWEI.debugTitle);
