41abac21339662d6f4ee3212c36f796c84d7a7e6
Jarek Koszuk <jkoszuk@gmail.com>
2023-03-04 21:28:59 +0100

    Fix Drag Ruler integration
    Fix system registration, speed calculation for non-player characters and
    creatures. Add speed names for EN, for other translations add English
    placeholders.

--- src/module/zweihander.js
+++ src/module/zweihander.js
@@ -102,35 +102,36 @@ Hooks.once("dragRuler.ready", (SpeedProvider) => {
     class zweihanderSpeedProvider extends SpeedProvider {
         get colors() {
             return [
-                {id: "maneuver", default: 0x1259b6, name: "zweihander.speeds.maneuver"},
-                {id: "hustle", default: 0x00FF00, name: "zweihander.speeds.hustle"},
-                {id: "charge", default: 0xFFFF00, name: "zweihander.speeds.charge"},
-                {id: "run", default: 0xFF8000, name: "zweihander.speeds.run"}
+                {id: "maneuver", default: 0x1259b6, name: "ZWEI.speeds.maneuver"},
+                {id: "hustle", default: 0x00FF00, name: "ZWEI.speeds.hustle"},
+                {id: "charge", default: 0xFFFF00, name: "ZWEI.speeds.charge"},
+                {id: "run", default: 0xFF8000, name: "ZWEI.speeds.run"}
             ];
         };
 
         getRanges(token) {
-			// MANEUVER: 1 yd
-			// HUSTLE: MOV
-			// CHARGE: MOV*2
-			// RUN: MOV*3
-			
-			const minSpeed = 1;
-            const baseSpeed = token.actor.system.stats.secondaryAttributes.movement.current;
-            const chargeSpeed = baseSpeed * 2;
-            const runSpeed = baseSpeed * 3;
-			
-            const ranges = [
-                {range: minSpeed, color: "maneuver"},
-                {range: baseSpeed, color: "hustle"},
-                {range: chargeSpeed, color: "charge"},
-                {range: runSpeed, color: "run"}
-            ];
+          // MANEUVER: 1 yd
+          // HUSTLE: MOV
+          // CHARGE: MOV*2
+          // RUN: MOV*3
+          const movement = token.actor.system.stats.secondaryAttributes.movement;
 
-            return ranges;
+          const minSpeed = 1;
+          const baseSpeed = movement.current ?? movement.value;
+          const chargeSpeed = baseSpeed * 2;
+          const runSpeed = baseSpeed * 3;
+
+          const ranges = [
+            { range: minSpeed, color: "maneuver" },
+            { range: baseSpeed, color: "hustle" },
+            { range: chargeSpeed, color: "charge" },
+            { range: runSpeed, color: "run" }
+          ];
+
+          return ranges;
         };
     }
-    dragRuler.registerModule("fvtt-module-drag-ruler-zweihander", zweihanderSpeedProvider);
+    dragRuler.registerSystem("zweihander", zweihanderSpeedProvider);
 })
 
 Hooks.once('init', async function () {
