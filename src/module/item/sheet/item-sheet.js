d1f8c09b6dafb0c506a7382438e91dd1c76a5e86
José Lozano <lozanoje@gmail.com>
2024-12-27 11:36:50 +0100

    Simplification of the code to search for ancestral traits table

--- src/module/item/sheet/item-sheet.js
+++ src/module/item/sheet/item-sheet.js
@@ -554,35 +554,34 @@ export default class ZweihanderItemSheet extends ItemSheet {
 
     if (item.type !== 'ancestry') return;
 
-    const worldTable = game.tables.find((table) => table.name.includes(item.name));
-    const isWorldTableUndefined = typeof worldTable === 'undefined';
-
     // If Roll Table doesn't exist in World, use Compendium as fallback. Only works for default Ancestries.
-    let compendiumTable;
+    let ancestralTraitsTable;
 
-    if (isWorldTableUndefined) {
-      const lang = game.i18n.lang;
-      const packLang = lang == 'de' ? '-de' : lang == 'es' ? '-es' : lang == 'fr' ? '-fr' : lang == 'ru' ? '-ru' : '';
-      const packName = game.i18n.localize('ITEM.TypeTrait').toLowerCase();
-      const compendiumpacks = Array.from(game.packs.keys()).filter((x) => x.includes(packLang) && x.includes('zh-charactercreation-tables'));
-      const tablesPackName = compendiumpacks[0] ? compendiumpacks[0] : 'zweihander.zh-charactercreation-tables';
-      const characterCreationPack = game.packs.get(tablesPackName);
-      const characterCreationPackIndex = await characterCreationPack.getIndex();
-      const compendiumTableEntry = characterCreationPackIndex.find((table) => {
-        return ZweihanderUtils.normalizedIncludes(table.name, item.name) && ZweihanderUtils.normalizedIncludes(table.name, packName); 
-      });
+    const lang = game.i18n.lang;
+    const packLang = lang == 'de' ? '-de' : lang == 'es' ? '-es' : lang == 'fr' ? '-fr' : lang == 'ru' ? '-ru' : '';
+    const packName = game.i18n.localize('ITEM.TypeTrait').toLowerCase();
+    const compendiumpacks = Array.from(game.packs.keys()).filter(
+      (x) => x.includes(packLang) && x.includes('zh-charactercreation-tables')
+    );
+    const tablesPackName = compendiumpacks[0];
+    const characterCreationPack = game.packs.get(tablesPackName);
+    const characterCreationPackIndex = await characterCreationPack.getIndex();
+    const ancestralTraitsTableEntry = characterCreationPackIndex.find((table) => {
+      return (
+        ZweihanderUtils.normalizedIncludes(table.name, item.name) &&
+        ZweihanderUtils.normalizedIncludes(table.name, packName)
+      );
+    });
 
-      compendiumTable = await characterCreationPack.getDocument(compendiumTableEntry?._id);
-    }
+    ancestralTraitsTable = await characterCreationPack.getDocument(ancestralTraitsTableEntry?._id);
 
-    const isCompendiumTableUndefined = typeof compendiumTable === 'undefined';
+    const isancestralTraitsTableUndefined = typeof ancestralTraitsTable === 'undefined';
 
-    if (isWorldTableUndefined && isCompendiumTableUndefined) {
+    if (isancestralTraitsTableUndefined) {
       ui.notifications.error(`${game.i18n.localize('ZWEI.othermessages.noancestrytable')}: ${item.name}`);
       return;
     }
 
-    const ancestralTraitsTable = isWorldTableUndefined ? compendiumTable : worldTable;
     const diceRoll = await ancestralTraitsTable.roll();
     const ancestralTraitsTableResult = await ancestralTraitsTable.draw({ roll: diceRoll });
 
