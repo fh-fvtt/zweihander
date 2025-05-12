748a1b5616f3c3507dffd91e3656d9e0c1fcb5bc
re4xn <sa_veliy@hotmail.com>
2025-05-12 12:42:44 +0100

    Add DTh calculation to NPCs

--- src/module/actor/entity/npc.js
+++ src/module/actor/entity/npc.js
@@ -1,3 +1,24 @@
 import ZweihanderCreature from './creature';
+import * as ZweihanderUtils from '../../utils';
 
-export default class ZweihanderNPC extends ZweihanderCreature {}
+export default class ZweihanderNPC extends ZweihanderCreature {
+  prepareDerivedData(actor) {
+    super.prepareDerivedData(actor);
+
+    const systemData = actor.system;
+    const sa = systemData.stats.secondaryAttributes;
+
+    if (!actor.system.stats.manualMode) {
+      const equippedArmor = actor.items.filter((a) => a.type === 'armor');
+
+      const maxEquippedArmor =
+        equippedArmor?.[ZweihanderUtils.argMax(equippedArmor.map((a) => a.system.damageThresholdModifier))];
+
+      const damageModifier = maxEquippedArmor?.system?.damageThresholdModifier ?? 0;
+
+      sa.damageThreshold.value = systemData.stats.primaryAttributes.brawn.bonus + damageModifier;
+      sa.damageThreshold.baseValue = systemData.stats.primaryAttributes.brawn.bonus;
+      sa.damageThreshold.dtm = damageModifier;
+    }
+  }
+}
